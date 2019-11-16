fbcabb390e5f38b362ef8482ea11eacad1e6dea7
Iza <kasza.izabela@gmail.com>
2019-11-16 19:22:43 +0100

    Deadline, Importance added

--- toDoList.js
+++ toDoList.js
@@ -1,24 +1,43 @@
+//Display current date
+let current = new Date();
+let dd = current.getDate();
+let mm = current.getMonth()+1;
+let yyyy = current.getFullYear();
+if (dd<10) {
+    dd='0'+dd;
+}
+if (mm<10) {
+    mm = '0'+mm;
+}
+document.querySelector('#current-date').innerHTML = `${dd}.${mm}.${yyyy}`;
+
 //Tasks list
-let listOfTasks = [];
+let listOfTasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 //Submit task
-let btn = document.getElementById('button');
-btn.addEventListener("click", addTask);
+let form = document.querySelector('.add-task');  
+form.addEventListener('submit', addTask);
 
 //Stops page reload & adds task to list
 function addTask(event) {
     event.preventDefault();
 
     let text = document.getElementById('to-do').value; 
+    let date = document.getElementById('deadline').value;
+    let imp = document.getElementById('importance').value;
     
     //Checking if sth was written
     if (text === '' || text.trim() === '') {
-        alert('You need to write Your task');
+        alert('You need to write down Your task');
+    } else if (Date.parse(date) < Date.parse(`${yyyy}-${mm}-${dd}`)) {
+        alert('Your deadline is in past!');
     } else {
         
-        //Create a task
+        //Creates a task
         const task = {
             text,
+            date,
+            imp,
             id: listOfTasks.length,
             checked: false
         }
@@ -31,17 +50,22 @@ function addTask(event) {
 
         //Makes input blank again
         document.getElementById('to-do').value = '';
+        document.getElementById('deadline').value = '';
+        document.getElementById('importance').value = 'Normal';
+
+        //Saving values in local storage
+        localStorage.setItem('tasks', JSON.stringify(listOfTasks));
     }
 }
 
-//Mark task as done/undone
+//Mark task as done/undone  ---After refresh desapires!!!!!!
 function done(task) {
     if (task.checked === false) {
         task.checked = true;
         console.log(`%c${task.text}`, 'text-decoration: line-through');
     }
 
-    listOfTasks.sort((a, b) => {return a.checked - b.checked});
+    return listOfTasks.sort((a, b) => {return a.checked - b.checked});
 }
 
 function undone(task) {
@@ -50,11 +74,63 @@ function undone(task) {
         console.log(`${task.text}`);
     }
 
-    listOfTasks.sort((a, b) => {
+    return listOfTasks.sort((a, b) => {
         if (a.checked === false && b.checked === false) {
             return a.id - b.id;
         }
     });
 }
 
+//Sort by importance
+function sortImp(a) {
+    if (a.checked === false) {
+        if (a.imp == 'Low') {
+            return 1;
+        } else if (a.imp == 'High') {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+//Sort by deadline
+function sortDeadline(a, b) {
+    if (a.checked === false && b.checked === false) { 
+        if (a.date === '' || b.date === '') {
+            return -1;
+        } else
+        if (a.date > b.date) {
+            return 1;
+        } else if (a.date < b.date) {
+            return -1;
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+}
+
+//Deadline date control
+function checkDeadline(listOfTasks) {
+    let past = 0;
+    let today = 0;
+    listOfTasks.forEach(function(n) {
+        if (n.date === '') {
+        } else if (n.date < `${yyyy}-${mm}-${dd}`) {
+            alert(`You should have finished ${n.text} by now!`);
+            past++;
+        } else if (n.date === `${yyyy}-${mm}-${dd}`) {
+            alert(`You should finish ${n.text} today!`);
+            today++;
+        }
+    });
+    return `You missed deadline of ${past} task(s) and should finish ${today} task(s) today.`
+} 
+
+
+//Search
+
+//Deleting task
 
